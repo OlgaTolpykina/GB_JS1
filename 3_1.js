d0595b04b6935ac59ec412f1ad58a4d6a24a4297
OlgaTolpykina <74467059+OlgaTolpykina@users.noreply.github.com>
2021-08-01 23:14:27 +0300

    Add files via upload

---
+++ 3_1.js
@@ -0,0 +1,11 @@
+'use strict';
+
+for (let i = 0; i <= 10; i++) {
+    if(i == 0) {
+        console.log('0-это ноль');
+    } else if(i % 2 == 1) {
+        console.log(i + '-нечетное число');
+    } else {
+        console.log(i + '-четное число');
+    }
+}
